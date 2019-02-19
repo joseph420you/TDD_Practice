25e124f19fc937f1c96b7778ae4861e729901d53
Joseph You <joseph420yu@gmail.com>
2019-02-19 17:10:19 +0800

    add senario: buy 2 books cost 190 dollars with 5% discount.

--- TDD_BOOKING/src/Cashier.java
+++ TDD_BOOKING/src/Cashier.java
@@ -2,7 +2,10 @@
  * Created by Joseph on 2019/2/19.
  */
 public class Cashier {
-    public int account(Books a) {
+    public int account(Books... books) {
+        if(books.length==2 && !books[0].equals(books[1])){
+            return (int)(100*2*0.95);
+        }
         return 100;
     }
 }
--- TDD_BOOKING/src/TestBooking.java
+++ TDD_BOOKING/src/TestBooking.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -6,10 +7,25 @@ import static org.junit.Assert.assertEquals;
  * Created by Joseph on 2019/2/19.
  */
 public class TestBooking {
+
+    private Cashier cashier;
+
+    @Before
+    public void init() {
+        this.cashier = new Cashier();
+    }
+
     @Test
-    public void buyOneBookCost100() {
-        Cashier cashier = new Cashier();
-        int cost = cashier.account(Books.A);
-        assertEquals(100, cost);
+    public void buy_One_Book_Cost_100() {
+        resultShouldBe(100, cashier.account(Books.A));
+    }
+
+    @Test
+    public void buy_2_Diff_Books_Cost_190_With_5_Per_Discount() {
+        resultShouldBe(190, cashier.account(Books.A, Books.B));
+    }
+
+    private void resultShouldBe(int expected, int cost) {
+        assertEquals(expected, cost);
     }
 }
