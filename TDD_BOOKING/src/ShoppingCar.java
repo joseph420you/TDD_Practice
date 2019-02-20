10664de4e383da00b30a4e1de271adba87455832
Joseph You <joseph420yu@gmail.com>
2019-02-20 14:35:00 +0800

    add missing class

---
+++ TDD_BOOKING/src/ShoppingCar.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Joseph on 2019/2/19.
+ */
+public class ShoppingCar {
+    private Set<Books> filter;
+
+    public ShoppingCar(Books... books) {
+        this.filter = new HashSet<Books>(Arrays.asList(books));
+    }
+
+    public double getDiscount() {
+        switch (filter.size()){
+            case 1:
+                return 1;
+            case 2:
+                return 0.95;
+            case 3:
+                return 0.90;
+            case 4:
+                return 0.80;
+            default:
+                return 1;
+        }
+    }
+
+    public int getDiffBookAmount() {
+        return filter.size();
+    }
+}
